9aa844e893ff9bfe3fddee0e45c6c1cac7c8e275
Maxime <maxime@manhattan.dedibox.fr>
2014-04-12 19:57:30 +0200

    Rien

--- config.js
+++ config.js
@@ -6,11 +6,7 @@ module.exports = {
   ip: '0.0.0.0',
   secret: 't32ç!567pyqs65dlkSDFfjbh',
   db : {
-<<<<<<< HEAD
     host : "localhost",
-=======
-    host : "storage.melinon.com",
->>>>>>> cb4573e2510f010f181c59fcb02b8552cf6193f7
     name : "lou_lt",
     sessionColl : "session",
   }
