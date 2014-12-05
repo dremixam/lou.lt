c6c21b4a1149de3ebca1cb58814574ab78aa69e2
Maxime Mélinon <maxime@melinon.fr>
2014-12-05 03:20:28 +0100

    Correction a l'arrache d'une faille

--- app/routes/channelJoin.js
+++ app/routes/channelJoin.js
@@ -13,6 +13,10 @@ module.exports = function (socket) {
       return;
     }
 
+          if ( typeof socket.handshake === 'undefined' ) {
+        return;
+      }
+
     var banlist = JSON.parse(data);
     var userIp = socket.handshake.headers['x-real-ip'] || socket.handshake.address.address;
 
